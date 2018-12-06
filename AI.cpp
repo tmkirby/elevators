cab384560cf73a637dbb89e621e1b9286f4b027a
tmkirby <44980246+tmkirby@users.noreply.github.com>
2018-12-06 17:14:17 -0500

    Update AI.cpp

--- AI.cpp
+++ AI.cpp
@@ -1,6 +1,26 @@
 #include "AI.h"
 #include <cassert>
 
+void GetAngerByFloor(BuildingState& buildingState, int totalAngerLevel[NUM_FLOORS]) {
+	for (int i = 0; i < NUM_FLOORS; i++) {
+		for (int j = 0; j < buildingState.floors[i].numPeople; j++) {
+			totalAngerLevel[i] += buildingState.floors[i].people[j].angerLevel;
+		}
+	}
+}
+
+int WhichElevatorShouldGo(BuildingState& buildingState, int floorNum) {
+	int diffInFloors = 9999999;
+	int elevatorToGo = -1;
+	for (int i = 0; i < NUM_ELEVATORS; i++) {
+		if ((buildingState.elevators[i].currentFloor - floorNum) < diffInFloors) {
+			diffInFloors = (buildingState.elevators[i].currentFloor - floorNum);
+			elevatorToGo = i;
+		}
+	}
+	return elevatorToGo;
+}
+
 Move getAIMove(const BuildingState& buildingState) {
     
     // TODO: finish getAIMove
